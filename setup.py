55c248b56041251cfb64b24e855ab81365f8db2d
monalippatil <monalippatil@yahoo.com>
2024-08-30 14:34:06 +1000

    ML Project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine Learning Data-Driven Insights: Predicting Cancer Mortality Rates Using Census Data in US Counties',
+    author='Monali',
+    license='MIT',
+)
